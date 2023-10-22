16c283df5223e29fcc2a5e3e8d1031f39b51c39d
zanzers <nowellsaavedra999@gmail.com>
2023-10-22 21:34:57 +0800

    Marcelot

---
+++ book.py
@@ -0,0 +1,22 @@
+# NOTE!!! w8 for the Online_store to uplaod...
+from Online_store import *
+
+class Book(Online_store):
+    def __init__(self, json_file, store_name):
+        super().__init__(json_file, store_name,)
+
+    def display_books(self):
+        data = books.read_data()
+ 
+        for book_data in data:
+             for key, value in book_data.items():
+                print(f"{key}: {value}")
+
+
+store = Online_store(json_file="books.json", store_name="MarsBook")
+books = Book('books.json', store)
+ 
+print(f" Store Name: {store.store_name}")
+
+
+books.display()
